2195ef1b4dc2a250a1fac010dd3b048520737ff1
C3NZ <cenz@cenz.io>
2019-02-10 15:39:34 -0800

    [feat] users can now "sign in" to the chat room

--- public/index.js
+++ public/index.js
@@ -1,4 +1,22 @@
+// Wait for the client's document to load before loading
+// any javascript
 $(document).ready(() => {
     // Connect to the server
     const socket = io.connect();
+    
+    // Handle when the user clicks the button
+    $('#createUserBtn').click((e) => {
+        e.preventDefault();
+        const username = $('#usernameInput').val();
+
+        // Make sure that the user has entered something for a name
+        if (username.length > 0) {
+            socket.emit('new user', username);
+            $('.usernameForm').remove();
+        }
+    });
+
+    socket.on('new user', (username) => {
+        console.log(`${username} has joined the chat!`);
+    })
 })
