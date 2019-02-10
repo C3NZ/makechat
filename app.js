fb5efd982548aecc2f465e78b89478c8273028e8
C3NZ <cenz@cenz.io>
2019-02-10 14:44:58 -0800

    [feat] configure server to handle socket connections

--- app.js
+++ app.js
@@ -3,23 +3,35 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const http = require('http');
+const socket = require('socket.io');
 
+// Register routers
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+// App configurations
 const app = express();
-const server = require('http').Server(app);
+const server = http.Server(app);
+const io = socket(server);
+
+// When a new connection occurs between our server and client
+io.on('connection', (socket) => {
+    console.log("NEW USER CONNECTED");
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Route middleware to be applied to every request
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Register custom routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -39,6 +51,7 @@ app.use((err, req, res, next) => {
     res.render('error');
 });
 
+// Set the server to start listening
 server.listen('3000', () => {
     console.log('Server listening on port 3000');
 });
