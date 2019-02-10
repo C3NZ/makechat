adac18d06959eeff6d9bf33c9604addd5920cfb1
C3NZ <cenz@cenz.io>
2019-02-10 15:38:50 -0800

    [feat] enable the server socket to emit an event notifying a user of when another user joins

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,4 +1,14 @@
 // chat sockets
+//
+// io.emit() - Sends data to all clients on the connection
+// socket.emit() - Sends data to the client that sent the original data to the server
+
+// Socket handling
 module.exports = (io, socket) => {
-    // Future socket listeners will go here
+    socket.on('new user', (username) => {
+        console.log(`${username} has joined the chat!`);
+
+        // Send the username to all clients currently connected
+        io.emit('new user', username)
+    });
 }
