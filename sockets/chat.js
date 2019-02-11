db918e4bebb46bb279f8241348021b3a8a04e8f1
C3NZ <cenz@cenz.io>
2019-02-10 18:14:56 -0800

    [feat] add a handler for handling when new messages are sent into the chat

--- sockets/chat.js
+++ sockets/chat.js
@@ -5,10 +5,17 @@
 
 // Socket handling
 module.exports = (io, socket) => {
+    // Handle a new user joining the chat
     socket.on('new user', (username) => {
         console.log(`${username} has joined the chat!`);
 
         // Send the username to all clients currently connected
         io.emit('new user', username)
     });
+
+    // Handle a new message being sent
+    socket.on('new message', (data) => {
+        console.log(`${data.sender}: ${data.message}`)
+        io.emit('new message', data);
+    })
 }
